a1135a65507351f799cd5f32f1270f5e9658fc27
Jacob Brenneman <j.brenneman19@gmail.com>
2015-07-09 13:25:05 -0600

    added app.js require

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(session({keys: [process.env.KEY1, provess.env.KEY2] }))
+app.use(session({keys: [process.env.KEY1, process.env.KEY2] }))
 app.use('/', routes);
 app.use('/users', users);
 
